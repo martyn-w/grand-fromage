c923fcab5046bbe13086daef28c806e5c39f7a7d
Martyn Whitwell <martyn.whitwell@gmail.com>
2018-04-21 11:29:01 +0100

    move files about

--- display/bin/docker-entrypoint.sh
+++ display/bin/docker-entrypoint.sh
@@ -5,7 +5,9 @@ echo "${TIMEZONE=UTC}" > /etc/timezone
 dpkg-reconfigure tzdata
 
 # Make sure i2c is loaded
+echo "starting modprobe"
 modprobe i2c-dev
+echo "finished modprobe"
 
 # Start the fuse driver
 systemctl start epd-fuse.service
