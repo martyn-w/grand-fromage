c5d2a06a678c906ffb0e4273447a66f93cb1fe03
Martyn Whitwell <martyn.whitwell@gmail.com>
2018-05-11 14:59:17 +0100

    wip

--- tracker/grandfromage/report.py
+++ tracker/grandfromage/report.py
@@ -8,27 +8,27 @@ import json
 
 class Report:
 
-    def __init__(self, report_msg):
-        self.report_msg = report_msg
+    def __init__(self, msg):
+        self.msg = json.loads(msg)
         self.report_class = self.get_report_class()
         self.is_tpv_fix = self.get_is_tpv_fix()
 
     def get_report_class(self):
-        if self.report_msg is not None and 'class' in self.report_msg:
-            return self.report_msg['class']
+        if self.msg is not None and 'class' in self.msg:
+            return self.msg['class']
         else:
             return None
 
     def get_is_tpv_fix(self):
         return (self.report_class == 'TPV'
-            and 'mode' in self.report_msg
-            and self.report_msg['mode'] >= 2)
+            and 'mode' in self.msg
+            and self.msg['mode'] >= 2)
 
     def lat_lon(self):
         if self.is_tpv_fix:
-            return (self.report_msg['lat'], self.report_msg['lon'])
+            return (self.msg['lat'], self.msg['lon'])
         else:
             return None
 
     def json(self):
-        return json.dumps(self.report_msg.__dict__)
+        return json.dumps(self.msg.__dict__)
--- tracker/grandfromage/tracker.py
+++ tracker/grandfromage/tracker.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 __author__ = 'Martyn Whitwell'
@@ -6,6 +7,7 @@ __version__ = '0.2.0'
 
 
 import sys
+import time
 from gps3.agps3threaded import AGPS3mechanism
 from gps3 import agps3
 import paho.mqtt.publish
@@ -37,18 +39,21 @@ class Tracker:
         # self.session = gps.gps()
         # self.session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-        self.previous_tpv = None
-        self.latest_tpv = None
+        self.previous_position = None
+        self.latest_position = None
         self.last_ping = None
 
-    # def significant_change(self):
-    #     if (self.previous_tpv is None and self.latest_tpv is not None):
-    #         return True
-    #     elif (self.previous_tpv is not None and self.latest_tpv is not None):
-    #         delta = geopy.distance.vincenty(self.previous_tpv.lat_lon(), self.latest_tpv.lat_lon()).meters
-    #         return delta > settings.TRACKER_SIGNIFICANT_DISTANCE_METERS
-    #     else:
-    #         return False
+    def significant_change(self):
+        if (self.previous_position is None and self.latest_position is not None):
+            return True
+        elif (self.previous_position is not None and self.latest_position is not None):
+            delta = geopy.distance.vincenty(self.previous_position.lat_lon(), self.latest_position.lat_lon()).meters
+            print("Delta Change: ", delta, delta > settings.TRACKER_SIGNIFICANT_DISTANCE_METERS)
+            return delta > settings.TRACKER_SIGNIFICANT_DISTANCE_METERS
+        else:
+            return False
+
+
     #
     # def significant_ping(self):
     #     if settings.TRACKER_PING_FREQUENCY <= 0:
@@ -60,28 +65,35 @@ class Tracker:
     #     else:
     #         return self.last_ping + datetime.timedelta(0,settings.TRACKER_PING_FREQUENCY) < datetime.datetime.now()
     #
-    # def process(self):
-    #     report = Report(self.session.next())
-    #     if report.is_tpv_fix:
-    #         self.latest_tpv = report
-    #         # note: to prevent significant changes from not being identified if the change between two recent
-    #         # measurements is less than the threshold, we should only set the previous_tpv WHEN a significant change is
-    #         # identified, i.e. NOT on every report
-    #         if self.significant_change():
-    #             paho.mqtt.publish.single(self.FULL_TOPIC_CHANGE, self.latest_tpv.json())
-    #             self.previous_tpv = self.latest_tpv
-    #
-    #         if self.significant_ping():
-    #             paho.mqtt.publish.single(self.FULL_TOPIC_PING, self.latest_tpv.json())
-    #             self.last_ping = datetime.datetime.now()
+    def process(self, data):
+        report = Report(data)
+        if report.is_tpv_fix:
+            self.latest_position = report
+            # note: to prevent significant changes from not being identified if the change between two recent
+            # measurements is less than the threshold, we should only set the previous_position WHEN a significant change is
+            # identified, i.e. NOT on every report
+            if self.significant_change():
+                paho.mqtt.publish.single(self.FULL_TOPIC_CHANGE, self.latest_position.json())
+                self.previous_position = self.latest_position
+
+            if self.significant_ping():
+                paho.mqtt.publish.single(self.FULL_TOPIC_PING, self.latest_position.json())
+                self.last_ping = datetime.datetime.now()
 
     def run(self):
-        for new_data in self.gps_socket:
-            if new_data:
-                self.data_stream.unpack(new_data)
-                print('Altitude = ', self.data_stream.alt)
-                print('Latitude = ', self.data_stream.lat)
-                print(self.data_stream)
+        for data in self.gps_socket:
+            if data:
+                self.process(data)
+
+                # self.data_stream.unpack(new_data)
+                # print('Altitude = ', self.data_stream.alt)
+                # print('Latitude = ', self.data_stream.lat)
+                # print('Longitude = ', self.data_stream.lon)
+                # print('Speed = ', self.data_stream.speed)
+                # print('Course = ', self.data_stream.track)
+                # print('Time = ', self.data_stream.time)
+            time.sleep(1.0)
+
 
 
         # while True:
@@ -93,8 +105,8 @@ class Tracker:
         #         quit()
         #     except StopIteration:
         #         self.session = None
-        #         self.previous_tpv = None
-        #         self.latest_tpv = None
+        #         self.previous_position = None
+        #         self.latest_position = None
         #         self.last_ping = None
         #         print "{} has terminated".format(self.FULL_TOPIC)
 
