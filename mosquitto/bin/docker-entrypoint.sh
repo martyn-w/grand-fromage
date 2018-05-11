cb76b0f26886ca03252158dbbe5799327855fb3e
Martyn Whitwell <martyn.whitwell@gmail.com>
2018-05-11 11:28:22 +0100

    more alpinisation

--- mosquitto/bin/docker-entrypoint.sh
+++ mosquitto/bin/docker-entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Set timezone
-echo "${TIMEZONE=UTC}" > /etc/timezone
-dpkg-reconfigure tzdata
-
 # Ensure correct permissions on mosquitto folders
 mkdir -p /mosquitto/log /mosquitto/data
 chown -Rf mosquitto:mosquitto /mosquitto/log /mosquitto/data
