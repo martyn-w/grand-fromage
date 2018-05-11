50fbb9ce6f846868d9386faa056b66efec36769b
Martyn Whitwell <martyn.whitwell@gmail.com>
2018-05-11 12:21:18 +0100

    try alpine for python

--- tracker/bin/docker-entrypoint.sh
+++ tracker/bin/docker-entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Set timezone
-echo "${TIMEZONE=UTC}" > /etc/timezone
-dpkg-reconfigure tzdata
 
 # Make sure i2c is loaded
-echo "starting modprobe"
-modprobe i2c-dev
-echo "finished modprobe"
+# echo "starting modprobe"
+# modprobe i2c-dev
+# echo "finished modprobe"
 
 
 
